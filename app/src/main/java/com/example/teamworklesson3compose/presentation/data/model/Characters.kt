5b3b2be7a1e064c2cdf03cd1e123808a05571514
user <iskakovkuttuubaj0207@gmail.com>
2024-04-29 17:51:54 +0600

    Сверстал дизайн western desert (пункт 7)

---
+++ app/src/main/java/com/example/teamworklesson3compose/presentation/data/model/Characters.kt
@@ -0,0 +1,10 @@
+package com.example.teamworklesson3compose.presentation.data.model
+
+data class Characters(
+    val name:String,
+    val age: Int,
+    val image: String,
+    val height: String,
+    val family: String,
+    val status: String
+)
