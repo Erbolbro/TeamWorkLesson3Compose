73f5ba6208221ef5785cb916145b8967c143541d
pubgkonfuz <mashanloskm@gmail.com>
2024-05-07 21:34:14 +0600

    Сделал слой presentation

---
+++ app/src/main/java/com/example/teamworklesson3compose/presentation/models/CharacterUIModel.kt
@@ -0,0 +1,13 @@
+package com.example.teamworklesson3compose.presentation.models
+
+data class CharacterUIModel(
+    val image: String,
+    val gender: String,
+    val species: String,
+    val created: String,
+    val name: String,
+    val id: Int,
+    val type: String,
+    val url: String,
+    val status: String
+)
